e52d73dcd175c8179d8ae6c9a28ab3f32552523c
SS <750899260@qq.com>
2020-10-15 21:07:46 +0800

    open some step

--- CASE1/auto_download.py
+++ CASE1/auto_download.py
@@ -35,51 +35,51 @@ from bs4 import BeautifulSoup
 print('Step 1 : Get DOI From URL is Begin')
 time_start = time.time()
 
-# url  = "https://library.seg.org/doi/book/10.1190/segeab.39"
-# res  = requests.get(url)
-# soup = BeautifulSoup(res.content,'lxml')
-# # print(soup.prettify()) #查看原地址的信息
+url  = "https://library.seg.org/doi/book/10.1190/segeab.39"
+res  = requests.get(url)
+soup = BeautifulSoup(res.content,'lxml')
+# print(soup.prettify()) #查看原地址的信息
 
-# # Step 1.1 : 读取单篇文章的DOI
-# fp = open('1_LIST_of_DOI.txt', 'w')
-# for i in soup.select('div[class="issue-item__doi"]'):
-    # s = i.get_text()+'\n';
-    # fp.write(s)
-# fp.close()
+# Step 1.1 : 读取单篇文章的DOI
+fp = open('1_LIST_of_DOI.txt', 'w')
+for i in soup.select('div[class="issue-item__doi"]'):
+    s = i.get_text()+'\n';
+    fp.write(s)
+fp.close()
 
 
-# # Step 1.2 : 读取单篇文章的SECTION并创建文件夹
-# filename='temp'
-# fp = open('1_LIST_of_Section.txt', 'w')
-# i_count=0
-# for i in soup.select('div[class="titled_issues"]'):
-    # i_count=i_count+1
-    # try:
-        # filename = i.h4.string+'\n'
-    # except:
-        # filename
+# Step 1.2 : 读取单篇文章的SECTION并创建文件夹
+filename='temp'
+fp = open('1_LIST_of_Section.txt', 'w')
+i_count=0
+for i in soup.select('div[class="titled_issues"]'):
+    i_count=i_count+1
+    try:
+        filename = i.h4.string+'\n'
+    except:
+        filename
 
-    # print(i_count,filename)
-    # fp.write(filename)
-# fp.close()
+    print(i_count,filename)
+    fp.write(filename)
+fp.close()
 
-# lines = open('1_LIST_of_Section.txt').readlines()
-# i_count=0
-# for s in lines:
-    # i_count=i_count+1
-    # try:
-        # s=s.replace("\n","")
-        # s=s.replace("/"," ")
-        # flag_dir=os.path.exists(s)
-        # if not flag_dir:
-            # os.mkdir(s)
-            # print("     mkdir ",s)
-    # except:
-        # s=str(i_count)
-        # flag_dir=os.path.exists(s)
-        # if not flag_dir:
-            # os.mkdir(s)
-            # print("     mkdir ",s)
+lines = open('1_LIST_of_Section.txt').readlines()
+i_count=0
+for s in lines:
+    i_count=i_count+1
+    try:
+        s=s.replace("\n","")
+        s=s.replace("/"," ")
+        flag_dir=os.path.exists(s)
+        if not flag_dir:
+            os.mkdir(s)
+            print("     mkdir ",s)
+    except:
+        s=str(i_count)
+        flag_dir=os.path.exists(s)
+        if not flag_dir:
+            os.mkdir(s)
+            print("     mkdir ",s)
 
 time_end = time.time()
 print('     Step 1 cost ',time_end-time_start,'s')
@@ -90,25 +90,25 @@ print('Step 1 : Get DOI From URL is Finish')
 print('Step 2 : Get URLs of Papers is Begin')
 time_start = time.time()
 
-# lines = open('1_LIST_of_DOI.txt').readlines()
-# fp = open('2_LIST_of_URLs_of_Papers.txt', 'w')
-# headers = {
-    # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-# }
+lines = open('1_LIST_of_DOI.txt').readlines()
+fp = open('2_LIST_of_URLs_of_Papers.txt', 'w')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+}
 
-# data = {
-    # 'poster': 'test',
-    # 'syntax': 'cpp',
-    # 'content': 'test',
-# }
+data = {
+    'poster': 'test',
+    'syntax': 'cpp',
+    'content': 'test',
+}
 
-# for s in lines:
-    # res = requests.post(url=s, headers=headers, data=data, allow_redirects=False)
-    # fp.write(res.headers['location']+'\n')
-    # print(res.headers['location']) #显示进度 好慢啊
+for s in lines:
+    res = requests.post(url=s, headers=headers, data=data, allow_redirects=False)
+    fp.write(res.headers['location']+'\n')
+    print(res.headers['location']) #显示进度 好慢啊
 
-# fp.close()
+fp.close()
 
 time_end = time.time()
 print('     Step 2 cost ',time_end-time_start,'s')
@@ -119,14 +119,14 @@ print('Step 2 : Get URLs of Papers is Finsh')
 print('Step 3 : Get URLs of PDF is Begin')
 time_start = time.time()
 
-# lines = open('2_LIST_of_URLs_of_Papers.txt').readlines()
+lines = open('2_LIST_of_URLs_of_Papers.txt').readlines()
 
-# fp = open('3_LIST_of_URLS_of_PDF.txt', 'w')
-# for s in lines:
-    # s = s.replace("library.seg.org/doi/", "library.seg.org/doi/epdf/")
-    # fp.write(s)
-    # # print(s)
-# fp.close()
+fp = open('3_LIST_of_URLS_of_PDF.txt', 'w')
+for s in lines:
+    s = s.replace("library.seg.org/doi/", "library.seg.org/doi/epdf/")
+    fp.write(s)
+    # print(s)
+fp.close()
 
 time_end = time.time()
 print('     Step 3 cost ',time_end-time_start,'s')
@@ -147,7 +147,7 @@ i_all     = len(lines)
 for s in lines:
     time_start_line=time.time()
     print('No.',i_count+1,'/', i_all,':   ')
-    time.sleep(5)
+    time.sleep(40)
     i_count      = i_count+1
     section_path = lines_section[i_count-1]
     section_path = section_path.replace("\n","")
@@ -163,6 +163,7 @@ for s in lines:
         filename = i.get_text()
         filename = filename.replace("/"," ")
         filename = filename.replace(":"," ")
+        filename = filename.replace("?"," ")
         filename = './'+section_path+'/'+ filename +".pdf"
         print('     path&title is : ',filename)
 
